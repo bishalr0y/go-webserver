b5c707ba70cba8cee864a197ec11238a060a4d32
Bishal Roy <bishalroy895@gmail.com>
2024-02-28 11:18:28 +0530

    fix

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/bishalr0y/go_webserver/config"
-	"github.com/bishalr0y/go_webserver/controller"
+	"github.com/bishalr0y/go_webserver/controllers"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -20,14 +20,14 @@ func main() {
 	db.AutoMigrate(&Todo{})
 
 	r := gin.Default()
-	controller.HelloWorld()
-	r.POST("/todos", controller.CreateTodo)
-	r.GET("/todos", controller.FetchAllTodos)
-	r.GET("/todos/:id", controller.FetchSingleTodo)
-	r.PUT("/todos/:id", controller.UpdateTodo)
-	r.DELETE("/todos/:id", controller.DeleteTodo)
+	controllers.HelloWorld()
+	r.POST("/todos", controllers.CreateTodo)
+	r.GET("/todos", controllers.FetchAllTodos)
+	r.GET("/todos/:id", controllers.FetchSingleTodo)
+	r.PUT("/todos/:id", controllers.UpdateTodo)
+	r.DELETE("/todos/:id", controllers.DeleteTodo)
 
-	r.POST("/todos/multiple", controller.CreateTodos)
+	r.POST("/todos/multiple", controllers.CreateTodos)
 
 	r.Run(":8888")
 }
