252dc85e62b3ac99c3cda9cb9ea0717d3e17e9c4
Bishal Roy <bishalroy895@gmail.com>
2024-02-28 11:02:52 +0530

    feat: seperated the controllers

--- controller/todos.go
+++ controller/todos.go
@@ -1,7 +1,110 @@
 package controller
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bishalr0y/go_webserver/config"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB = config.ConnectToDb()
+
+type Todo struct {
+	gorm.Model
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
 
 func HelloWorld() {
 	fmt.Println("Hello world from controller")
+}
+
+func CreateTodo(c *gin.Context) {
+	var todo Todo
+	c.BindJSON(&todo)
+	err := db.Create(&todo)
+	if err.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the todo"})
+		return
+	}
+	c.JSON(200, todo)
+}
+
+func CreateTodos(c *gin.Context) {
+	var todos []Todo
+	c.BindJSON(&todos)
+	fmt.Println(todos)
+
+	for _, todo := range todos {
+		err := db.Create(&todo)
+		if err.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the todo", "todo": todo})
+			return
+		}
+	}
+
+	c.JSON(200, todos)
+}
+
+func FetchAllTodos(c *gin.Context) {
+	var todos []Todo
+	err := db.Find(&todos)
+	if err.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch all todos"})
+		return
+	}
+	c.JSON(200, todos)
+}
+
+func FetchSingleTodo(c *gin.Context) {
+	var todo Todo
+	id := c.Params.ByName("id")
+	err := db.First(&todo, id)
+	if err.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch the todo"})
+	}
+	c.JSON(200, todo)
+}
+
+func UpdateTodo(c *gin.Context) {
+	var todo Todo
+	id := c.Params.ByName("id")
+
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id paramter not found"})
+		return
+	}
+
+	err := db.First(&todo, id)
+
+	if err.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to find todo with that id"})
+		return
+	}
+
+	c.BindJSON(&todo)
+	err = db.Save(&todo)
+	if err.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update the todo"})
+		return
+	}
+	c.JSON(200, todo)
+}
+
+func DeleteTodo(c *gin.Context) {
+	id := c.Params.ByName("id")
+
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id paramter not found"})
+		return
+	}
+
+	var todo Todo
+	err := db.Where("id = ?", id).Delete(&todo)
+	if err.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the todo"})
+	}
+	c.JSON(200, gin.H{"id #" + id: "deleted"})
 }
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/bishalr0y/go_webserver/config"
 	"github.com/bishalr0y/go_webserver/controller"
 	"github.com/gin-gonic/gin"
@@ -18,114 +15,19 @@ type Todo struct {
 
 var db *gorm.DB
 
-// var err error
-
 func main() {
-	// dsn := "user=admin password=admin dbname=my_db port=5432 sslmode=disable TimeZone=Asia/Kolkata host=localhost"
-	// db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	panic("failed to connect database")
-	// }
 	db = config.ConnectToDb()
 	db.AutoMigrate(&Todo{})
 
 	r := gin.Default()
 	controller.HelloWorld()
-	r.POST("/todos", createTodo)
-	r.GET("/todos", fetchAllTodos)
-	r.GET("/todos/:id", fetchSingleTodo)
-	r.PUT("/todos/:id", updateTodo)
-	r.DELETE("/todos/:id", deleteTodo)
+	r.POST("/todos", controller.CreateTodo)
+	r.GET("/todos", controller.FetchAllTodos)
+	r.GET("/todos/:id", controller.FetchSingleTodo)
+	r.PUT("/todos/:id", controller.UpdateTodo)
+	r.DELETE("/todos/:id", controller.DeleteTodo)
 
-	r.POST("/todos/multiple", createTodos)
+	r.POST("/todos/multiple", controller.CreateTodos)
 
 	r.Run(":8888")
-}
-
-func createTodo(c *gin.Context) {
-	var todo Todo
-	c.BindJSON(&todo)
-	err := db.Create(&todo)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the todo"})
-		return
-	}
-	c.JSON(200, todo)
-}
-
-func createTodos(c *gin.Context) {
-	var todos []Todo
-	c.BindJSON(&todos)
-	fmt.Println(todos)
-
-	for _, todo := range todos {
-		err := db.Create(&todo)
-		if err.Error != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the todo", "todo": todo})
-			return
-		}
-	}
-
-	c.JSON(200, todos)
-}
-
-func fetchAllTodos(c *gin.Context) {
-	var todos []Todo
-	err := db.Find(&todos)
-	if err.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch all todos"})
-		return
-	}
-	c.JSON(200, todos)
-}
-
-func fetchSingleTodo(c *gin.Context) {
-	var todo Todo
-	id := c.Params.ByName("id")
-	err := db.First(&todo, id)
-	if err.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch the todo"})
-	}
-	c.JSON(200, todo)
-}
-
-func updateTodo(c *gin.Context) {
-	var todo Todo
-	id := c.Params.ByName("id")
-
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "id paramter not found"})
-		return
-	}
-
-	err := db.First(&todo, id)
-
-	if err.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to find todo with that id"})
-		return
-	}
-
-	c.BindJSON(&todo)
-	err = db.Save(&todo)
-	if err.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update the todo"})
-		return
-	}
-	c.JSON(200, todo)
-}
-
-func deleteTodo(c *gin.Context) {
-	id := c.Params.ByName("id")
-
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "id paramter not found"})
-		return
-	}
-
-	var todo Todo
-	err := db.Where("id = ?", id).Delete(&todo)
-	if err.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the todo"})
-	}
-	c.JSON(200, gin.H{"id #" + id: "deleted"})
 }
